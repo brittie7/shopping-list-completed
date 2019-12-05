522947c4017202b3c511f3084334a870a59168c1
Brittany Coleman <brittie7@gmail.com>
2019-12-05 13:39:48 -0500

    Removed notes

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-//shopping-item is the class name for apple, oranges, milk and bread
-// console.log($('button'));
-// $('button').click(function(){
-//     alert('Button Clicked');
-// });
-
-
-/*$(document).ready(function(){ 
-$("button").click(function(){     
-    alert('Button Clicked')  }); });
-
-    cmd / to comment out in bulk
-
-
- <form id="js-shopping-list-form">
-      <label for="shopping-list-entry">Add an item</label>
-      <input type="text" name="shopping-list-entry" id="shopping-list-entry" placeholder="e.g., broccoli">
-      <button id='test' type="submit">Add item</button>
-    </form>
-    
-    <ul class="shopping-list">
-      <li>
-        <span class="shopping-item">apples</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>
-
-
-    */
-
    $(function() {
     $('#js-shopping-list-form').submit(event =>{
         event.preventDefault();
